e2705ee9c81629af998e577d53f177735ae04ca4
LXD <liuxinda@buaa.edu.cn>
2018-12-07 23:15:59 +0800

    almost rewrite the code

--- losses/LiftedStructure.py
+++ losses/LiftedStructure.py
@@ -2,96 +2,68 @@ from __future__ import absolute_import
 
 import torch
 from torch import nn
-from torch.autograd import Variable
 import numpy as np
-
-
-def similarity(inputs_):
-    # Compute similarity mat of deep feature
-    # n = inputs_.size(0)
-    sim = torch.matmul(inputs_, inputs_.t())
-    return sim
+import math
 
 class LiftedStructureLoss(nn.Module):
-    def __init__(self, alpha=10, beta=2, margin=0.5, hard_mining=None, **kwargs):
+    def __init__(self, margin=1, hard_mining=None, **kwargs):
         super(LiftedStructureLoss, self).__init__()
         self.margin = margin
-        self.alpha = alpha
-        self.beta = beta
+        #self.alpha = alpha
         self.hard_mining = hard_mining
 
     def forward(self, inputs, targets):
         n = inputs.size(0)
-        # Compute similarity matrix
-        sim_mat = similarity(inputs)
+
+        mag = (inputs ** 2).sum(1).expand(n, n)
+        sim_mat = inputs.mm(inputs.transpose(0, 1))
+    
+        dist_mat = (mag + mag.transpose(0, 1) - 2 * sim_mat)
+        dist_mat = torch.nn.functional.relu(dist_mat).sqrt().cuda()
         # print(sim_mat)
         targets = targets.cuda()
         # split the positive and negative pairs
-        eyes_ = Variable(torch.eye(n, n)).cuda()
+        eyes_ = torch.tensor(torch.eye(n, n)).cuda()
+        zeros_ = torch.zeros(n,n).cuda()
         # eyes_ = Variable(torch.eye(n, n))
         pos_mask = targets.expand(n, n).eq(targets.expand(n, n).t())
         neg_mask = eyes_.eq(eyes_) - pos_mask
         pos_mask = pos_mask - eyes_.eq(1)
 
-        pos_sim = torch.masked_select(sim_mat, pos_mask)
-        neg_sim = torch.masked_select(sim_mat, neg_mask)
+        pos_dist = torch.where(pos_mask, dist_mat, zeros_)
+        neg_dist = torch.where(neg_mask, dist_mat, zeros_)
 
-        num_instances = len(pos_sim)//n + 1
-        num_neg_instances = n - num_instances
+        loss = 0
 
-        pos_sim = pos_sim.resize(len(pos_sim)//(num_instances-1), num_instances-1)
-        neg_sim = neg_sim.resize(
-            len(neg_sim) // num_neg_instances, num_neg_instances)
+        len_p = pos_mask.nonzero().shape[0]
+        if self.hard_mining is not None:
+            for ind in torch.triu(pos_mask).nonzero():
+                i = ind[0].item()
+                j = ind[1].item()
 
-        # base = (torch.mean(pos_sim) + torch.mean(neg_sim) + 1).data[0]/3
-        base = 0.5
-        # print('base is:', base)
+                #neg_ik_component = torch.sum(torch.exp(self.margin - torch.topk(neg_dist[i][neg_dist[i].nonzero()].squeeze(),\
+                #                                                             math.ceil(self.alpha*len(neg_dist[i].nonzero())))))
+                #neg_jl_component = torch.sum(torch.exp(self.margin - torch.topk(neg_dist[j][neg_dist[j].nonzero()].squeeze(),\
+                #                                                             math.ceil(self.alpha*len(neg_dist[j].nonzero())))))
+                hardest_k, _ = torch.topk(neg_dist[i][neg_dist[i].nonzero()].squeeze(), 1, largest=False)
+                neg_ik_component = torch.sum(torch.exp(self.margin - hardest_k))
+                hardest_l, _ = torch.topk(neg_dist[j][neg_dist[j].nonzero()].squeeze(), 1, largest=False)
+                neg_jl_component = torch.sum(torch.exp(self.margin - hardest_l))
+                Jij_heat = torch.log(neg_ik_component+neg_jl_component) + pos_dist[i,j]
+                loss += torch.nn.functional.relu(Jij_heat) ** 2
+            return loss / len_p
+        
+        else:
+            for ind in torch.triu(pos_mask).nonzero():
+                i = ind[0].item()
+                j = ind[1].item()
+                neg_ik_component = torch.sum(torch.exp(self.margin - neg_dist[i][neg_dist[i].nonzero()].squeeze()))
+                neg_jl_component = torch.sum(torch.exp(self.margin - neg_dist[j][neg_dist[j].nonzero()].squeeze()))
+                Jij_heat = torch.log(neg_ik_component+neg_jl_component) + pos_dist[i,j]
 
-        loss = list()
-        c = 0
-        scale_value = self.margin
+                loss += torch.nn.functional.relu(Jij_heat) ** 2
 
-        for i, pos_pair_ in enumerate(pos_sim):
-            # print(i)
-            pos_pair_ = torch.sort(pos_pair_)[0]
-            neg_pair_ = torch.sort(neg_sim[i])[0]
-
-            if self.hard_mining is not None:
-                # print(scale_value)
-                # print(self.alpha,  self.beta, self.margin)
-                neg_pair = torch.masked_select(neg_pair_, neg_pair_ + 0.1 > pos_pair_[0])
-                pos_pair = torch.masked_select(pos_pair_, pos_pair_ - 0.1 <  neg_pair_[-1])
-                # pos_pair = pos_pair[1:]
-                
-                # if len(pos_pair) > 1:
-                #     pos_pair = torch.masked_select(pos_pair, pos_pair > torch.mean(neg_pair_))
-                # if len(neg_pair) > 1:
-                #     neg_pair = torch.masked_select(neg_pair, neg_pair > pos_pair_[-1] - 0.05)  
-                if len(neg_pair) < 1 or len(pos_pair) < 1:
-                    c += 1
-                    continue
-
-                pos_loss =  torch.log(torch.sum(torch.exp(-(pos_pair - base))))
-                neg_loss =  torch.log(torch.sum(torch.exp( (neg_pair - base))))
-                # pos_loss = 2.0/self.beta * torch.log(1 + torch.sum(torch.exp(-self.beta*(pos_pair - self.margin))))
-                # neg_loss = 2.0/self.alpha * torch.log(1 + torch.sum(torch.exp(self.alpha*(neg_pair - self.margin))))
-                loss.append(pos_loss + neg_loss)
-
-            else:
-                # print('no-Hard mining')
-                neg_pair = neg_pair_
-                pos_pair = pos_pair_
-                pos_loss = torch.log(torch.sum(-torch.exp((pos_pair - self.margin))))
-                neg_loss = torch.log(torch.sum(torch.exp((neg_pair - self.margin))))
-                loss.append(pos_loss + neg_loss)
-
-
-        loss = torch.sum(torch.cat(loss))/n
-        prec = float(c)/n
-        neg_d = torch.mean(neg_sim).data[0]
-        pos_d = torch.mean(pos_sim).data[0]
-
-        return loss, prec, pos_d, neg_d
+            return loss / len_p
 
 
 def main():
@@ -100,18 +72,21 @@ def main():
     output_dim = 2
     num_class = 4
     # margin = 0.5
-    x = Variable(torch.rand(data_size, input_dim), requires_grad=False)
+    # torch.manual_seed(123)
+    # torch.cuda.manual_seed_all(123)
+    # np.random.seed(123)
+    x = torch.tensor(torch.rand(data_size, input_dim), requires_grad=False)
     # print(x)
-    w = Variable(torch.rand(input_dim, output_dim), requires_grad=True)
+    w = torch.tensor(torch.rand(input_dim, output_dim), requires_grad=True)
     inputs = x.mm(w)
-    y_ = 8*list(range(num_class))
-    targets = Variable(torch.IntTensor(y_))
+    #y_ = 8*list(range(num_class))
+    y_ = np.random.choice(num_class, data_size)
+    targets = torch.tensor(torch.IntTensor(y_))
 
-    print(LiftedStructureLoss()(inputs, targets))
+    print(LiftedStructureLoss(hard_mining=1)(inputs, targets))
+    print()
 
 
 if __name__ == '__main__':
     main()
     print('Congratulations to you!')
-
-
